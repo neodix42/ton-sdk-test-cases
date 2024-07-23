0b07379f8373737b19c07675d96f54cd79643182
neodiX <neodix42@ton.org>
2024-07-23 20:31:46 +0200

    adding wallets v3r2 and v4r3 tests - WIP

--- src/test/java/UsageExampleTest.java
+++ src/test/java/UsageExampleTest.java
@@ -13,6 +13,7 @@ import org.ton.java.tlb.types.Message;
 import org.ton.java.utils.Utils;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -24,182 +25,104 @@ public class UsageExampleTest {
 
     public static final String TON_TEST_CASES_SMARTCONTRACTS = "smartcontracts.json";
     public static final String TON_TEST_CASES_CRYPTOGRAPHY = "cryptography.json";
-
     Gson gson = new GsonBuilder().setObjectToNumberStrategy(ToNumberPolicy.LONG_OR_DOUBLE).create();
 
     @Test
-    public void testWalletV3R2Creation() throws IOException {
+    public void testSmartContracts6() throws IOException {
 
         // read the JSON file with tests cases
         String fileContentWithUseCases = new String(Files.readAllBytes(Paths.get(TON_TEST_CASES_SMARTCONTRACTS)));
         TonSdkTestCases tonSdkTestCases = gson.fromJson(fileContentWithUseCases, TonSdkTestCases.class);
 
-        String testId = "smartcontracts-9";
+        String testId = "smartcontracts-6";
         // select particular test case by category name and test id
         TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
 
-        // fetch test's description and id. It's always good to show test id, since it is unique across all tests.
+        log.info("testId: {}", testId);
+
+        String privateKey = testCase.getInput().get("privateKey").toString();
+        Long workchain = (Long) testCase.getInput().get("workchain");
+        String destinationAddress = testCase.getInput().get("destinationAddress").toString();
+        Long walletId = (Long) testCase.getInput().get("walletId");
+        Long seqno = (Long) testCase.getInput().get("seqNo");
+        BigDecimal amountTonCoins = new BigDecimal(testCase.getInput().get("amountTonCoins").toString());
+        Boolean bounceFlag = (Boolean) testCase.getInput().get("bounceFlag");
+        Long validUntil = (Long) testCase.getInput().get("validUntil");
+        Long sendMode = (Long) testCase.getInput().get("sendMode");
+        String comment = testCase.getInput().get("comment").toString();
+
+        byte[] secretKey = Utils.hexToSignedBytes(privateKey);
+        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(secretKey);
+
+        WalletV3R2 contract = WalletV3R2.builder()
+                .wc(workchain)
+                .keyPair(keyPair)
+                .walletId(walletId)
+                .build();
+
+        String expectedBocAsHex = (String) testCase.getExpectedOutput().get("externalMessageBocAsHexWithCrc");
+
+        WalletV3Config config = WalletV3Config.builder()
+                .walletId(walletId)
+                .seqno(seqno)
+                .destination(Address.of(destinationAddress))
+                .amount(Utils.toNano(amountTonCoins))
+                .validUntil(validUntil)
+                .bounce(bounceFlag)
+                .mode(sendMode.intValue())
+                .comment(comment)
+                .build();
+        Message sendMsg = contract.prepareExternalMsg(config);
+        assertThat(sendMsg.toCell().toHex(true).toUpperCase()).isEqualTo(expectedBocAsHex);
+    }
+
+    @Test
+    public void testSmartContracts7() throws IOException {
+
+        // read the JSON file with tests cases
+        String fileContentWithUseCases = new String(Files.readAllBytes(Paths.get(TON_TEST_CASES_SMARTCONTRACTS)));
+        TonSdkTestCases tonSdkTestCases = gson.fromJson(fileContentWithUseCases, TonSdkTestCases.class);
+
+        String testId = "smartcontracts-7";
+
+        // select particular test case by category name and test id
+        TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
+
         String description = testCase.getDescription();
 
         log.info("testId: {}", testId);
         log.info("description: {}", description);
 
-        // fetch input parameters and print test case details
-        String prvKey = (String) testCase.getInput().get("privateKey");
+        String privateKey = testCase.getInput().get("privateKey").toString();
         Long workchain = (Long) testCase.getInput().get("workchain");
         Long walletId = (Long) testCase.getInput().get("walletId");
+        Long initialSeqno = (Long) testCase.getInput().get("initialSeqno");
 
-        log.info("input parameters:");
-        log.info("  prvKey: {}", prvKey);
-        log.info("  workchain: {}", workchain);
-        log.info("  walletId: {}", walletId);
-
-        // test the functionality of your library
-
-        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(Utils.hexToSignedBytes(prvKey));
+        byte[] secretKey = Utils.hexToSignedBytes(privateKey);
+        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(secretKey);
 
         WalletV3R2 contract = WalletV3R2.builder()
                 .wc(workchain)
                 .keyPair(keyPair)
                 .walletId(walletId)
+                .initialSeqno(initialSeqno)
                 .build();
 
-        String actualRawAddress = contract.getAddress().toRaw();
-
-        Message msg = contract.prepareDeployMsg();
-
-        String actualExtMsgForSerializationAsBoc = Utils.bytesToHex(msg.toCell().toBoc(true)).toUpperCase();
-
-        // fetch the expected result and compare it against the actual one
-        String expectedRawAddress = (String) testCase.getExpectedOutput().get("rawAddress");
-        String expectedBocAsHex = (String) testCase.getExpectedOutput().get("bocAsHex");
-
-        log.info("expected results:");
-        log.info("  rawAddress: {}", expectedRawAddress);
-        log.info("  bocAsHex: {}", expectedBocAsHex);
-
-        assertThat(actualRawAddress).isEqualTo(expectedRawAddress);
-        assertThat(actualExtMsgForSerializationAsBoc).isEqualTo(expectedBocAsHex);
-    }
-
-    @Test
-    public void testWalletV3R2Transfer() throws IOException {
-
-        // read the JSON file with tests cases
-        String fileContentWithUseCases = new String(Files.readAllBytes(Paths.get(TON_TEST_CASES_SMARTCONTRACTS)));
-        TonSdkTestCases tonSdkTestCases = gson.fromJson(fileContentWithUseCases, TonSdkTestCases.class);
-
-        String testId = "smartcontracts-10";
-
-        // select particular test case by category name and test id
-        TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
-
-        // print test case details
-        log.info("TestCase: {}", testCase);
-
-        // fetch input parameters
-        String prvKey = (String) testCase.getInput().get("privateKey");
-        Long workchain = (Long) testCase.getInput().get("workchain");
-        Long walletId = (Long) testCase.getInput().get("walletId");
-        String destinationAddress = (String) testCase.getInput().get("destinationAddress");
-        Long seqNo = (Long) testCase.getInput().get("seqNo");
-        Long amountTonCoins = (Long) testCase.getInput().get("amountTonCoins");
-        Boolean bounceFlag = (Boolean) testCase.getInput().get("bounceFlag");
-        Long validUntil = (Long) testCase.getInput().get("validUntil");
-        Long sendMode = (Long) testCase.getInput().get("sendMode");
-
-
-        // test the functionality of your library
-
-        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(Utils.hexToSignedBytes(prvKey));
-
-        WalletV3R2 contract = WalletV3R2.builder()
-                .wc(workchain)
-                .keyPair(keyPair)
-                .walletId(walletId)
-                .build();
-
-        WalletV3Config config = WalletV3Config.builder()
-                .destination(Address.of(destinationAddress))
-                .walletId(walletId)
-                .seqno(seqNo)
-                .amount(Utils.toNano(amountTonCoins))
-                .bounce(bounceFlag)
-                .validUntil(validUntil)
-                .mode(Math.toIntExact(sendMode)) // default is 3, can be omitted
-                .build();
-
-        Message msg = contract.prepareExternalMsg(config);
-
-        String actualExtMsgForSerializationAsBoc = Utils.bytesToHex(msg.toCell().toBoc(true)).toUpperCase();
-
-        // fetch the expected result and compare it against the actual one
-        String expectedBocAsHex = (String) testCase.getExpectedOutput().get("externalMessageBocAsHex");
-
-        assertThat(actualExtMsgForSerializationAsBoc).isEqualTo(expectedBocAsHex);
-    }
-
-
-    @Test
-    public void testWalletV3R2CreationMaster() throws IOException {
-
-        // read the JSON file with tests cases
-        String fileContentWithUseCases = new String(Files.readAllBytes(Paths.get(TON_TEST_CASES_SMARTCONTRACTS)));
-        TonSdkTestCases tonSdkTestCases = gson.fromJson(fileContentWithUseCases, TonSdkTestCases.class);
-
-        String testId = "smartcontracts-13";
-
-        // select particular test case by category name and test id
-        TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
-
-        // print test case details
-        log.info("TestCase: {}", testCase);
-
-        // fetch input parameters
-        String prvKey = (String) testCase.getInput().get("privateKey");
-        Long workchain = (Long) testCase.getInput().get("workchain");
-        Long walletId = (Long) testCase.getInput().get("walletId");
-
-
-        // test the functionality of your library
-
-        TweetNaclFast.Signature.KeyPair keyPair = TweetNaclFast.Signature.keyPair_fromSeed(Utils.hexToSignedBytes(prvKey));
-
-        WalletV3R2 contract = WalletV3R2.builder()
-                .wc(workchain)
-                .keyPair(keyPair)
-                .walletId(walletId)
-                .build();
-
-        String actualCodeAsHex = contract.getStateInit().getCode().bitStringToHex();
-        String actualDataAsHex = contract.getStateInit().getData().bitStringToHex();
-        String actualRawAddress = contract.getAddress().toRaw();
-
-        Message msg = contract.prepareDeployMsg();
-
-        String actualExtMsgAsHex = msg.toCell().bitStringToHex();
-
-        String actualExtMsgAsBoc = Utils.bytesToHex(msg.toCell().toBoc(true)).toUpperCase();
-
-        // fetch the expected result and compare it against the actual one
         String expectedRawAddress = (String) testCase.getExpectedOutput().get("rawAddress");
         String expectedCodeAsHex = (String) testCase.getExpectedOutput().get("codeAsHex");
         String expectedDataAsHex = (String) testCase.getExpectedOutput().get("dataAsHex");
-        String expectedExtMsgAsHex = (String) testCase.getExpectedOutput().get("externalMessageAsHex");
-        String expectedBocAsHex = (String) testCase.getExpectedOutput().get("bocAsHex");
+        String expectedExternalMessageAsHex = (String) testCase.getExpectedOutput().get("externalMessageAsHex");
+        String expectedBocAsHex = (String) testCase.getExpectedOutput().get("externalMessageBocAsHexWithCrc");
 
-        log.info("expected results:");
-        log.info("  rawAddress: {}", expectedRawAddress);
-        log.info("  codeAsHex: {}", expectedCodeAsHex);
-        log.info("  dataAsHex: {}", expectedDataAsHex);
-        log.info("  extMsgAsHex: {}", expectedExtMsgAsHex);
-        log.info("  bocAsHex: {}", expectedBocAsHex);
+        String rawAddress = contract.getAddress().toRaw();
+        assertThat(rawAddress).isEqualTo(expectedRawAddress);
 
-        assertThat(actualRawAddress).isEqualTo(expectedRawAddress);
-        assertThat(actualCodeAsHex).isEqualTo(expectedCodeAsHex);
-        assertThat(actualDataAsHex).isEqualTo(expectedDataAsHex);
-        assertThat(actualExtMsgAsHex).isEqualTo(expectedExtMsgAsHex);
-        assertThat(actualExtMsgAsBoc).isEqualTo(expectedBocAsHex);
+        assertThat(contract.createCodeCell().bitStringToHex().toUpperCase()).isEqualTo(expectedCodeAsHex);
+        assertThat(contract.createDataCell().bitStringToHex().toUpperCase()).isEqualTo(expectedDataAsHex);
+        Message msg = contract.prepareDeployMsg();
+
+        assertThat(msg.toCell().bitStringToHex().toUpperCase()).isEqualTo(expectedExternalMessageAsHex);
+        assertThat(msg.toCell().toHex(true).toUpperCase()).isEqualTo(expectedBocAsHex);
     }
 
     @Test
