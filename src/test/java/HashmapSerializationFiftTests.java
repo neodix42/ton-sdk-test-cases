a8899c3fc2af851a3800366942239cbfdf815b81
neodiX <neodix@ton.org>
2024-09-19 13:57:38 +0400

    added hashmap-serialization fift tests

--- src/test/java/HashmapSerializationFiftTests.java
+++ src/test/java/HashmapSerializationFiftTests.java
@@ -35,6 +35,7 @@ public class HashmapSerializationFiftTests {
     public static void setUpBeforeClass() throws URISyntaxException {
         log.info("executing fift script, might take long time...");
         URL resource = TestFiftRunner.class.getResource("/fift-tests/hashmap-serialization.fif");
+        assert resource != null;
         File fiftFile = Paths.get(resource.toURI()).toFile();
         FiftRunner fiftRunner = FiftRunner.builder().build();
         String absolutePath = fiftFile.getAbsolutePath();
@@ -42,11 +43,11 @@ public class HashmapSerializationFiftTests {
     }
 
     @Test
-    public void testHashmapSerialization1() {
+    public void testHashmapSerialization4() {
 
         log.info("{}", result);
 
-        String testId = "hashmap-serialization-1";
+        String testId = "hashmap-serialization-4";
         // select particular test case by category name and test id
         TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
 
@@ -56,9 +57,65 @@ public class HashmapSerializationFiftTests {
         log.info("testId: {}", testId);
         log.info("description: {}", description);
 
-        String actualResult1 = UtilsStr.sb(result, "hashmap-serialization-1-cellPrint: ", "end").replaceAll("\\r", "").trim();
-        String actualResult2 = UtilsStr.sb(result, "hashmap-serialization-1-cellHash: ", "\n").trim();
-        String actualResult3 = UtilsStr.sb(result, "hashmap-serialization-1-cellBocWithCrc: ", "\n").trim();
+        String actualResult1 = UtilsStr.sb(result, "hashmap-serialization-4-cellPrint: ", "end").replaceAll("\\r", "").trim();
+        String actualResult2 = UtilsStr.sb(result, "hashmap-serialization-4-cellHash: ", "\n").trim();
+        String actualResult3 = UtilsStr.sb(result, "hashmap-serialization-4-cellBocWithCrc: ", "\n").trim();
+
+        String expectedResultCellPrint = (String) testCase.getExpectedOutput().get("cellPrint");
+        String expectedResultCellHash = (String) testCase.getExpectedOutput().get("cellHash");
+        String expectedResultCellBocWithCrc = (String) testCase.getExpectedOutput().get("cellBocWithCrc");
+
+        assertThat(actualResult1).isEqualTo(expectedResultCellPrint);
+        assertThat(actualResult2).isEqualTo(expectedResultCellHash.toUpperCase());
+        assertThat(actualResult3).isEqualTo(expectedResultCellBocWithCrc);
+    }
+
+    @Test
+    public void testHashmapSerialization5() {
+
+        log.info("{}", result);
+
+        String testId = "hashmap-serialization-5";
+        // select particular test case by category name and test id
+        TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
+
+        // fetch test's description and id. It's always good to show test id, since it is unique across all tests.
+        String description = testCase.getDescription();
+
+        log.info("testId: {}", testId);
+        log.info("description: {}", description);
+
+        String actualResult1 = UtilsStr.sb(result, "hashmap-serialization-5-cellPrint: ", "end").replaceAll("\\r", "").trim();
+        String actualResult2 = UtilsStr.sb(result, "hashmap-serialization-5-cellHash: ", "\n").trim();
+        String actualResult3 = UtilsStr.sb(result, "hashmap-serialization-5-cellBocWithCrc: ", "\n").trim();
+
+        String expectedResultCellPrint = (String) testCase.getExpectedOutput().get("cellPrint");
+        String expectedResultCellHash = (String) testCase.getExpectedOutput().get("cellHash");
+        String expectedResultCellBocWithCrc = (String) testCase.getExpectedOutput().get("cellBocWithCrc");
+
+        assertThat(actualResult1).isEqualTo(expectedResultCellPrint);
+        assertThat(actualResult2).isEqualTo(expectedResultCellHash.toUpperCase());
+        assertThat(actualResult3).isEqualTo(expectedResultCellBocWithCrc.toUpperCase());
+    }
+
+    @Test
+    public void testHashmapSerialization8() {
+
+        log.info("{}", result);
+
+        String testId = "hashmap-serialization-8";
+        // select particular test case by category name and test id
+        TonSdkTestCases.TestCase testCase = tonSdkTestCases.getTestCases().get(testId);
+
+        // fetch test's description and id. It's always good to show test id, since it is unique across all tests.
+        String description = testCase.getDescription();
+
+        log.info("testId: {}", testId);
+        log.info("description: {}", description);
+
+        String actualResult1 = UtilsStr.sb(result, "hashmap-serialization-8-cellPrint: ", "end").replaceAll("\\r", "").trim();
+        String actualResult2 = UtilsStr.sb(result, "hashmap-serialization-8-cellHash: ", "\n").trim();
+        String actualResult3 = UtilsStr.sb(result, "hashmap-serialization-8-cellBocWithCrc: ", "\n").trim();
 
         String expectedResultCellPrint = (String) testCase.getExpectedOutput().get("cellPrint");
         String expectedResultCellHash = (String) testCase.getExpectedOutput().get("cellHash");
